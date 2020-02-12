ea8947df2be0b84d93d92314ea8586d73272c840
unknown <king03.create@gmail.com>
2020-02-12 17:40:13 +0900

    ver3

--- changed_push.bat
+++ changed_push.bat
@@ -1,6 +1,6 @@
 set USR_INPUT_STR=
 set /P USR_INPUT_STR="please set char : "
 git add Maiwai3
-git commit -m USR_INPUT_STR
+git commit -m %USR_INPUT_STR%
 git push -u origin master
 set /P USR_INPUT_STR="please set char : "
